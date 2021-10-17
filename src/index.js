f128c559d205459df70b7c078ed083f38046c35f
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2021-10-17 20:37:28 +0300

    Updates HTML

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import menuCard from './templates/menu-items.hbs';
+import foodItems from './menu.json';
 import './sass/main.scss';
+
+console.log(menuCard);
+
+const menuContainer = document.querySelector('.js-menu');
+
+const cardsMarkup = createMenuCardsMarkup(foodItems);
+
+menuContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createMenuCardsMarkup(foodItems) {
+  return foodItems.map(menuCard).join('');
+}
