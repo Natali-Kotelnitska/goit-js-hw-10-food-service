57082c7738e3379872a3e35473dfa97a963f5f0f
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2021-10-17 23:43:14 +0300

    Updates JS

--- src/index.js
+++ src/index.js
@@ -2,14 +2,34 @@ import menuCard from './templates/menu-items.hbs';
 import foodItems from './menu.json';
 import './sass/main.scss';
 
-console.log(menuCard);
-
 const menuContainer = document.querySelector('.js-menu');
-
 const cardsMarkup = createMenuCardsMarkup(foodItems);
 
 menuContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
 function createMenuCardsMarkup(foodItems) {
-  return foodItems.map(menuCard).join('');
+  return menuCard(foodItems);
+}
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+  body: document.querySelector('body'),
+  themeSwitcher: document.querySelector('#theme-switch-toggle'),
+};
+
+refs.themeSwitcher.addEventListener('change', onThemeChange);
+
+function onThemeChange(evt) {
+  localStorage.setItem('theme', 'Theme.LIGHT');
+
+  if (evt.target.checked) {
+    refs.body.classList.remove(Theme.LIGHT);
+    return refs.body.classList.add(Theme.DARK);
+  }
+  refs.body.classList.add(Theme.LIGHT);
+  refs.body.classList.remove(Theme.DARK);
 }
