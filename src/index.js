491667b70ff1bb9cbf458824a103c77b37d32228
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2021-10-19 01:52:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,18 +17,20 @@ const Theme = {
 };
 
 let darkMode = localStorage.getItem('Theme');
+document.body.classList.add(Theme.LIGHT);
+
 const refs = {
   body: document.querySelector('body'),
   themeSwitcher: document.querySelector('#theme-switch-toggle'),
 };
 
 const setDarkMode = () => {
-  document.body.classList.add(Theme.DARK);
+  refs.body.classList.replace(Theme.LIGHT, Theme.DARK);
   localStorage.setItem('Theme', Theme.DARK);
 };
 
 const setLightMode = () => {
-  refs.body.classList.remove(Theme.DARK);
+  refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
   localStorage.setItem('Theme', Theme.LIGHT);
 };
 
